196bc17cd1fd8b44698a2e7f36fbe9541a868e93
nicetech-creator <toptech20190609@gmail.com>
2021-09-01 01:45:51 +0800

    fix: struct management

--- contracts/TheNFT.sol
+++ contracts/TheNFT.sol
@@ -1,7 +1,8 @@
-pragma solidity 0.8.0;
+pragma solidity 0.8.2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TheNFT is ERC721, Ownable {
   using Counters for Counters.Counter;
@@ -26,14 +27,14 @@ contract TheNFT is ERC721, Ownable {
   mapping (uint => Bid) public bids;
 
   // events
-  event OfferCreated(uint indexed tokenID, uint minValue);
-  event OfferWithdrawed(uint indexed tokenID);
-  event DealMade(uint indexed tokenID, uint value, address indexed fromAddress, address indexed toAddress);
-  event BidEntered(uint indexed tokenID, uint value, address indexed fromAddress);
-  event BidWithdrawed(uint indexed tokenID);
+  event OfferCreated(uint indexed tokenId, uint minValue);
+  event OfferWithdrawed(uint indexed tokenId);
+  event DealMade(uint indexed tokenId, uint value, address indexed fromAddress, address indexed toAddress);
+  event BidEntered(uint indexed tokenId, uint value, address indexed fromAddress);
+  event BidWithdrawed(uint indexed tokenId);
 
-  constructor(string memory _name, string memory _symbol, string memory _baseURI) ERC721(_name, _symbol) {
-    baseURI = _baseURI;
+  constructor(string memory _name, string memory _symbol, string memory _nftURI) ERC721(_name, _symbol) {
+    baseURI = _nftURI;
   }
 
   function mint(address to) external onlyOwner { 
@@ -44,32 +45,40 @@ contract TheNFT is ERC721, Ownable {
   }
 
   function makeOffer(uint256 tokenId, uint256 minPrice) external {
-    require(_owners[tokenId] == msg.sender, "TheNFT: not_owner");
+    require(ownerOf(tokenId) == msg.sender, "TheNFT: not_owner");
     require(minPrice > 0, "TheNFT: not_valid_price");
     offers[tokenId] = Offer(minPrice, true);
     emit OfferCreated(tokenId, minPrice);
   }
 
-  function acceptOffer(uint256 tokenID) external payable {
-    Offer storage offer = offers[tokenID];
+  function acceptOffer(uint256 tokenId) external payable {
+    Offer storage offer = offers[tokenId];
 
     require(offer.isForSale == true, 'TheNFT: non_existing_offer');
-    require(msg.value >= offer.minPrice, 'TheNFT: not_enough_fund');
+    require(msg.value >= offer.minValue, 'TheNFT: not_enough_fund');
 
-    payable address seller = payable(_owners[tokenId]);
+    address payable seller = payable(ownerOf(tokenId));
     seller.transfer(msg.value);
 
     // remove offer
-    offer = Offer(0, false);
+    offer.minValue = 0;
+    offer.isForSale = false;
 
-    _transfer(seller, msg.sender, tokenID);
+    _transfer(seller, msg.sender, tokenId);
 
-    emit DealMade(tokenID, msg.value, seller, msg.sender);
+    emit DealMade(tokenId, msg.value, seller, msg.sender);
   }
 
   function withdrawOffer(uint256 tokenId) external {
-    require(_owners[tokenId] == msg.sender, "TheNFT: not_owner");
-    offer = Offer(0, false);
+    require(ownerOf(tokenId) == msg.sender, "TheNFT: not_owner");
+
+    Offer storage offer = offers[tokenId];
+
+    require(offer.isForSale == true, "TheNFT: not_exist_offer");
+
+    offer.minValue = 0;
+    offer.isForSale = false;
+
     emit OfferWithdrawed(tokenId);
   }
 
@@ -83,25 +92,27 @@ contract TheNFT is ERC721, Ownable {
     // refund to the old bidder
     _refundBid(tokenId);
 
-    oldBid = Bid(msg.sender, msg.value);
+    oldBid.bidder = msg.sender;
+    oldBid.value = msg.value;
 
-    emit BidEntered(tokenID, msg.value, msg.address);
+    emit BidEntered(tokenId, msg.value, msg.sender);
   }
 
   function acceptBid(uint256 tokenId) external {
-    require(_owners[tokenId] == msg.sender, "TheNFT: not_owner");
+    require(ownerOf(tokenId) == msg.sender, "TheNFT: not_owner");
 
     Bid storage bid = bids[tokenId];
     require(bid.bidder != address(0), "TheNFT: not_exist_bid");
 
     _transfer(msg.sender, bid.bidder, tokenId);
     // transfer fund
-    msg.sender.transfer(bid.value);
+    payable(msg.sender).transfer(bid.value);
 
-    emit DealMade(tokenID, bid.value, msg.sender, bid.bidder);
+    emit DealMade(tokenId, bid.value, msg.sender, bid.bidder);
 
     // remove bid
-    bid = Bid(address(0), 0);
+    bid.bidder = address(0);
+    bid.value = 0;
   }
 
   function withdrawBid(uint256 tokenId) external {
@@ -109,9 +120,11 @@ contract TheNFT is ERC721, Ownable {
 
     require(bid.bidder == msg.sender, "TheNFT: not_bidder");
 
-    _refundBid(tokenID);
+    _refundBid(tokenId);
 
-    bid = Bid(address(0), 0);
+    // remove bid
+    bid.bidder = address(0);
+    bid.value = 0;
 
     emit BidWithdrawed(tokenId);
   }
@@ -128,7 +141,7 @@ contract TheNFT is ERC721, Ownable {
    * token will be the concatenation of the `baseURI` and the `tokenId`. Empty
    * by default, can be overriden in child contracts.
    */
-  function _baseURI() internal view virtual returns (string memory) {
+  function _baseURI() internal view virtual override returns (string memory) {
     return baseURI;
   }
 
